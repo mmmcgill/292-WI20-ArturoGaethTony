dfd7466b7999b03123f4936c4dc2fb9780f92639
Arturo Jimenez <ajimenez2@knox.edu>
2020-02-01 17:12:25 -0600

    Version 2!
    Added animations for the squirrels idle and moving animations. Added moving animations for the bird. Worked on the main menu with buttons "working". Need to get other menus done for it to be plausible.

---
+++ CS292-Template/Assets/Scripts/ExitGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitGame : MonoBehaviour
+{
+
+    private Button quitButton;
+
+    void Start()
+    {
+        quitButton = GetComponent<Button>();
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    void QuitGame()
+    {
+        Debug.Log("Game closed!");
+        Application.Quit();
+    }
+}
