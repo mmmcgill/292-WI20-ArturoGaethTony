ea0f60ec7161f01c4462f0a720cb04610773b3de
Anthony Reyes <randomhabit@hotmail.com>
2020-02-02 18:15:48 -0600

    Player Script Update, Tile Map Collision
    Nutty moves, Will only move Vertical or Horizontal, not both at the same time. Tile Map water collision has been added, colliders edited to fit the water. Respawn function added

---
+++ CS292-Template/Assets/Scripts/playerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public int health;
+    private Vector3 spawnPos;
+    Rigidbody2D rigidbody2d;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 5;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        spawnPos = transform.position;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 position = rigidbody2d.position;
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        }
+        else if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+
+            position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        }
+        rigidbody2d.MovePosition(position);
+    }
+    
+    void respawnNutty()
+    {
+
+        rigidbody2d.MovePosition(spawnPos);
+    }
+}
