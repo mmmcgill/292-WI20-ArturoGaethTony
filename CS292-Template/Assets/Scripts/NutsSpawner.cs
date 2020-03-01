6d8b9f8c44b697f7799386b7ab6c0acf1ce605f6
Anthony Reyes <randomhabit@hotmail.com>
2020-03-01 12:44:21 -0600

    Misc changes & tutorial

--- CS292-Template/Assets/Scripts/NutsSpawner.cs
+++ CS292-Template/Assets/Scripts/NutsSpawner.cs
@@ -36,7 +36,7 @@ public class NutsSpawner : MonoBehaviour
     public void generateNut(){
         //x= random -2.2f and 2.3f
         //y= random -3.3f and 4.2f
-        Vector2 nutPos = new Vector2(Random.Range(-2.2f, 2.3f), Random.Range(-3.3f, 4.2f));
+        Vector2 nutPos = new Vector2(Random.Range(-2.2f, 2.3f), (yBase + Random.Range(-5, 5) * 0.755f));
         GameObject nutClone=Instantiate(nut, nutPos, Quaternion.identity);
         
     }
